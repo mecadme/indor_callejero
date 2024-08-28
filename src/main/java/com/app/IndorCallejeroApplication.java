6091f82eb57b32ae32644f5cf5135a0791372a90
Mauro Cadme <mecadme@hotmail.com>
2024-08-28 18:12:35 -0500

    repo initialization

---
+++ src/main/java/com/app/IndorCallejeroApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndorCallejeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndorCallejeroApplication.class, args);
+	}
+
+}
